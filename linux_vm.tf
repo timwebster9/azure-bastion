573ae5bb0984ca5e3e70758e01c4425c4a0b8890
Tim Webster <tim.webster@contino.io>
2021-09-04 20:53:56 +0100

    fix SKU

--- linux_vm.tf
+++ linux_vm.tf
@@ -38,7 +38,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 }
