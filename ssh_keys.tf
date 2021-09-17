df957630a0c0632c9658cb0e629cc2fab01869b6
Tim Webster <tim.webster@contino.io>
2021-09-18 00:07:53 +0100

    fix

--- ssh_keys.tf
+++ ssh_keys.tf
@@ -9,7 +9,8 @@ resource "null_resource" "ssh_keys" {
     provisioner "local-exec" {
         command = <<EOT
           ssh-keygen -m PEM -t rsa -b 4096 -P "" -f ./id_rsa
-          az keyvault secret set --name cmdline-ssh-private-key --vault-name bastionkv675674475675 --value ./id_rsa
+          az keyvault secret set --name cmdline-ssh-private-key --vault-name bastionkv675674475675 --file ./id_rsa
+          az keyvault secret set --name cmdline-ssh-public-key --vault-name bastionkv675674475675 --file ./id_rsa.pub
     EOT  
     }
 }
